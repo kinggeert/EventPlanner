4e8801c98281d0993d8b1a50160f3cbfb33aa69f
kinggeert <geertbruls@outlook.com>
2024-10-09 16:37:05 +0200

    Added CategoriesController.cs

--- EventPlanner/Data/EventDb.cs
+++ EventPlanner/Data/EventDb.cs
@@ -48,4 +48,6 @@ public class EventDb : DbContext
         });
 
     }
+
+public DbSet<EventPlanner.Models.Category> Category { get; set; } = default!;
 }
--- EventPlanner/Program.cs
+++ EventPlanner/Program.cs
@@ -30,7 +30,7 @@ public class Program
 
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+            pattern: "{controller=Events}/{action=Index}/{id?}");
 
         app.Run();
     }
